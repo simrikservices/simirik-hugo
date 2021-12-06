b30b820f92019d849bff540f7c818180076909e6
Sudeep Gurung <sudeepgurung.sg@gmail.com>
2021-12-06 14:56:05 +0545

    wip

--- postcss.config.js
+++ postcss.config.js
@@ -13,7 +13,19 @@ module.exports = {
         'h3',
         'h4',
         'ul',
-        'li'
+        'li',
+        'i',
+        'fas',
+        'fa-plane',
+        'fa-user-shield',
+        'fa-hard-hat',
+        'fa-id-card-alt',
+        'fa-school',
+        'fa-store',
+        'fa-home',
+        'fa-hotel',
+        'fa-hospital',
+        'fa-dog'
       ]
     },
     autoprefixer: {},
